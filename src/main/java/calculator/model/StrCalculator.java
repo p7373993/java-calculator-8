a19ec6b964cc451ec29950b36cd53653a85c0095
sungwoo <p7373993@gmail.com>
2025-10-20 04:11:56 +0900

    refactor(Calculator) : 연속,마지막 구분자 예외 처리
    validToken 메서드 내 예외처리 추가

--- src/main/java/calculator/model/StrCalculator.java
+++ src/main/java/calculator/model/StrCalculator.java
@@ -28,7 +28,7 @@ public class StrCalculator {
             numbers = numbers.substring(nl + 1);
         }
 
-        String[] tokens = numbers.split(deliRegex);
+        String[] tokens = numbers.split(deliRegex, -1);
         int sum = 0;
         for (String t : tokens) {
             int num = Integer.parseInt(t);
@@ -39,16 +39,23 @@ public class StrCalculator {
     }
 
     private void validToken(String token) {
+
+        //연속, 마지막 구분자(ex)"3::2","3:"
+        if (token.isEmpty()) {
+            throw new IllegalArgumentException("연속, 마지막 구분자 사용 금지");
+        }
         //공백 오류
         if (token.contains(" ")) {
             throw new IllegalArgumentException("문자열 내 공백 사용 금지 ");
         }
 
+        //양수가 아닐시 오류
         int num = Integer.parseInt(token);
         if (num <= 0) {
             throw new IllegalArgumentException("양수가 아닙니다");
         }
 
+        //문자열내 구분자,숫자 아닌 문자 포함시 오류
         if (!token.matches("[1-9][0-9]*")) {
             throw new IllegalArgumentException("비허용 문자 사용");
         }
