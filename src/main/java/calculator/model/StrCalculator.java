c8f756630ea06eeb7421a2bc26dfd59b2361d0d1
sungwoo <p7373993@gmail.com>
2025-10-20 01:02:31 +0900

    fix(Calculator) : 기본 문법자로 나누는 기능 수정
    C스타일 코드 경고 변경
    String tokens[] -> String[] tokens

--- src/main/java/calculator/model/StrCalculator.java
+++ src/main/java/calculator/model/StrCalculator.java
@@ -6,7 +6,7 @@ public class StrCalculator {
             return 0;//공백은 0반환
         }
 
-        String tokens[] = input.split("[,:]");
+        String[] tokens = input.split("[,:]");
         int sum = 0;
         for (String t : tokens) {
             sum += Integer.parseInt(t);
