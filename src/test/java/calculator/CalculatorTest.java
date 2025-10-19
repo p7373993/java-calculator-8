9fbd5bba50f7dcdc5eb8aefaf8e1a7b5e3a16e02
sungwoo <p7373993@gmail.com>
2025-10-20 02:44:02 +0900

    test(test) : 커스텀 구분자 테스트 코드 작성
    test 폴더내 CalculatorTest 클래스 생성

---
+++ src/test/java/calculator/CalculatorTest.java
@@ -0,0 +1,51 @@
+package calculator;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.Test;
+
+public class CalculatorTest extends NsTest {
+
+    @Test
+    void 기본_구분자_사용() {
+        assertSimpleTest(() -> {
+            run("2:3");
+            assertThat(output()).contains("결과 : 5");
+        });
+    }
+
+
+    @Test
+    void 커스텀_구분자_사용() {
+        assertSimpleTest(() -> {
+            run("//;\\n1");
+            assertThat(output()).contains("결과 : 1");
+        });
+    }
+
+    @Test
+    void 커스텀_구분자_사용2() {
+        assertSimpleTest(() -> {
+            run("//;\\n1;2");
+            assertThat(output()).contains("결과 : 3");
+        });
+    }
+
+    @Test
+    void 예외_테스트() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("-1,2,3"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+
+    @Override
+    public void runMain() {
+        Application.main(new String[]{});
+    }
+
+}
